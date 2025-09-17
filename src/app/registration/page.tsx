6952796e74daecef0937d6b0bde852f78bef98e2
Daniel Herrera <admin@digammastudio.com>
2025-09-16 22:01:25 -0500

    fix: logo image

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -5,7 +5,7 @@ export default function RegistrationPage() {
 	return (
 		<main className='min-h-screen bg-secondary py-8'>
 			<div className='mb-4 mt-4 mx-8 flex justify-between items-center'>
-				<Image src="/images/logo.png" alt="Logo Arrobamedellín" width={200} height={100} />
+				<Image src="https://storage.googleapis.com/arroba-downloads/recursos/logo.png" alt="Logo Arrobamedellín" width={200} height={100} />
 				<h1 className='mb-6 text-center text-2xl font-bold text-white'>
 					Inscripciones Sapiencia {new Date().getFullYear()} - Convocatoria 9
 				</h1>

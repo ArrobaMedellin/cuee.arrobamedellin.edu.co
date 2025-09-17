11546a6fa7ee031d90ad67b8ac6724f4df4c6d07
Daniel Herrera <admin@digammastudio.com>
2025-09-16 21:45:14 -0500

    feat: Basepath

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: '/registration',
+  assetPrefix: '/registration',
+  trailingSlash: true,
   async redirects() {
     return [
       {

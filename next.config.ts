0136ec6b32465dc54c4a2e11a5f8d7f664399372
Daniel Andrés Herrera Rodríguez <dherrerr@gmail.com>
2025-10-27 14:31:41 -0500

    fix: new path

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  // basePath: '/registration',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'storage.cloud.google.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'fonts.gstatic.com',
-      },
-    ],
-  },
-};
+	basePath: '/registration',
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'storage.cloud.google.com'
+			},
+			{
+				protocol: 'https',
+				hostname: 'storage.googleapis.com'
+			},
+			{
+				protocol: 'https',
+				hostname: 'fonts.gstatic.com'
+			}
+		]
+	}
+}
 
-export default nextConfig;
+export default nextConfig

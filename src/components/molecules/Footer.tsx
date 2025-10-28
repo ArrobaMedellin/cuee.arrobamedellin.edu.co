028f9c6f7d0a685bcc5ebdd5235ba39c4a7cd8b7
Daniel Andrés Herrera Rodríguez <dherrerr@gmail.com>
2025-10-28 14:12:48 -0500

    feat: Logos

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -4,7 +4,7 @@ export default function Foooter() {
 	return (
 		<footer className='flex justify-end text-center text-sm text-white p-0 px-8 mt-auto'>
 			<Image
-				src='/images/logo_sapiencia.png'
+				src='https://storage.googleapis.com/gestor-reports/logo_sapiencia.png'
 				alt='Logo Arrobamedellín'
 				width={300}
 				height={200}
--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Header() {
 	return (
 		<div className='mb-4 pt-8 mx-8 flex justify-between items-center'>
-			<Image src="/images/logo.png" alt="Logo Arrobamedellín" width={200} height={100} />
+			<Image
+				src='https://storage.googleapis.com/gestor-reports/logo.png'
+				alt='Logo Arrobamedellín'
+				width={200}
+				height={100}
+			/>
 			<h1 className='mb-6 text-center text-2xl font-bold text-white'>
 				Inscripciones Sapiencia {new Date().getFullYear()}
 			</h1>
